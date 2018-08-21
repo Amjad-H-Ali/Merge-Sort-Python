7b9a9dee949b44a0252f33d13174fe50bc88740a
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-08-20 20:05:12 -0500

    Defined Variable to equal center index of array

--- merge.py
+++ merge.py
@@ -5,8 +5,14 @@ def merge_sort(array):
  if(len(array) < 2):
  	return array
 
+ center = round(len(array)/2, 0) 
+ 
+ return center	
+
 
 print(merge_sort([1, 2, 3, 4]))
+
+print(merge_sort([1, 2, 3, 4, 5]))
 
 print(merge_sort([1, 2]))
 
