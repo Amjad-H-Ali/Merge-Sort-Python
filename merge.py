9344ca4bf30785310a8b46d59de7b6930f432899
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-08-20 21:10:07 -0500

    Using the spread operator, we populate a remainder array with whatever is left in the sub arrays, then we populate another array with values in result and remainder; Complete

--- merge.py
+++ merge.py
@@ -18,7 +18,7 @@ def merge_sort(array):
 def merge(left_array, right_array):
 	result, left_indx, right_indx = [], 0, 0
 
-	while(left_indx < len(left_array) && right_indx < len(right_array)):
+	while(left_indx < len(left_array) and right_indx < len(right_array)):
 
 		if(left_array[left_indx] < right_array[right_indx]):
 
@@ -28,7 +28,11 @@ def merge(left_array, right_array):
 		else:
 			result.append(right_array[right_indx])
 
-			right += 1	
+			right_indx += 1
+
+	remainder = [*left_array[left_indx:], *right_array[right_indx:]]
+
+	return [*result, *remainder]		
 
 
 
@@ -42,3 +46,5 @@ print(merge_sort([1, 2, 3, 4, 5]))
 print(merge_sort([1, 2]))
 
 print(merge_sort([1]))
+
+print(merge_sort([6, 0, 0, 2, 32, 78, 9, 0, 19, 34, 1]))
