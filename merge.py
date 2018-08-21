1a0f2f85cbe7888a6b130bc53507c8eda13e5fae
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-08-20 20:20:07 -0500

    Defined variable to equal left side of array

--- merge.py
+++ merge.py
@@ -5,9 +5,11 @@ def merge_sort(array):
  if(len(array) < 2):
  	return array
 
- center = round(len(array)/2, 0) 
+ center = int(len(array)/2)
+
+ left = array[:center]
  
- return center	
+ return left	
 
 
 print(merge_sort([1, 2, 3, 4]))
