1bbb976a8a5ed1f26c2faa79f58814d9a36723f4
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-08-20 19:54:41 -0500

    Function returns array with len less than 2; This will be our exit code for the recursion process

--- merge.py
+++ merge.py
@@ -3,11 +3,11 @@ print('Merge Sort!')
 def merge_sort(array):
  
  if(len(array) < 2):
- 	print(array)
+ 	return array
 
 
-merge_sort([1, 2, 3, 4])
+print(merge_sort([1, 2, 3, 4]))
 
-merge_sort([1, 2])
+print(merge_sort([1, 2]))
 
-merge_sort([1])
+print(merge_sort([1]))
