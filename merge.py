cd204fe4172f42a5909e544207fc8005c1b195e6
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-08-20 20:55:55 -0500

    Defined vairable for left and right index and new array

--- merge.py
+++ merge.py
@@ -1,22 +1,28 @@
 print('Merge Sort!')
 
 def merge_sort(array):
+	if(len(array) < 2):
+		return array
+
+	center = int(len(array)/2)
+	
+	left = array[:center]
+
+	right = array[center:]
  
- if(len(array) < 2):
- 	return array
+	return merge( merge_sort(left), merge_sort(right))
 
- center = int(len(array)/2)
 
- left = array[:center]
 
- right = array[center:]
- 
- return merge(
- 	merge_sort(left),
- 	merge_sort(right)
- )
 
- def merge(left_array, right_array)	
+def merge(left_array, right_array):
+	result, left_indx, right_indx = [], 0, 0
+
+	print(result)
+
+
+
+
 
 
 print(merge_sort([1, 2, 3, 4]))
